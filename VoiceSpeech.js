14f247b832cd8d8c40f431e57c1a8aef60a8dff1
Amay Kataria <amkatari@microsoft.com>
2019-01-17 01:20:06 +0530

    Speech changes. I like this speech.

--- VoiceSpeech.js
+++ VoiceSpeech.js
@@ -2,8 +2,8 @@ class VoiceSpeech {
     constructor(speechStarted, speechEnded) {
         this.myVoice = new p5.Speech('Google हिन्दी'); 
         this.myVoice.setVolume(1); 
-        this.myVoice.setRate(0.8);
-        this.myVoice.setPitch(0.7);
+        this.myVoice.setRate(0.77);
+        this.myVoice.setPitch(0.92);
         this.myVoice.utterance.onerror = this.error; 
         this.myVoice.onStart = speechStarted; 
         this.myVoice.onEnd = speechEnded;
