2a1419faf17cee57251178f123b2d9e40528ad1f
Amay Kataria <amkatari@microsoft.com>
2019-11-27 15:21:57 -0600

    Change Font size

--- src/CenterTitle.js
+++ src/CenterTitle.js
@@ -28,7 +28,7 @@ class CenterTitle {
         this.el.style("opacity", this.curOpacity);
         this.el.style("font-family", "'Indie Flower', cursive");
         this.el.style("letter-spacing", '5px');
-        this.el.style('font-size', '90px');
+        this.el.style('font-size', '110px');
         this.el.style("color", "#ffbf00");
         this.el.style('display', 'flex');
         this.el.style("align-items", "center");
@@ -48,7 +48,7 @@ class CenterTitle {
         this.startAppButton.size(this.finalSize.x, this.finalSize.y); 
         this.startAppButton.style('background-image', 'linear-gradient(to bottom right, #5E0000, #800000)');
         this.startAppButton.style("color", "#ffbf00");
-        this.startAppButton.style('font-size', '60px');
+        this.startAppButton.style('font-size', '100px');
         this.startAppButton.style('font-family', "'Indie Flower', cursive");
         this.startAppButton.mousePressed(() => { // This format binds this pointer
             this.startAppButton.hide();
