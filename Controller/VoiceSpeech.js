5136acf4873c85e306550a0072308f87df228f36
Amay Kataria <amkatari@microsoft.com>
2019-11-27 15:09:30 -0600

    Slightly modify the agent's sound

--- Controller/VoiceSpeech.js
+++ Controller/VoiceSpeech.js
@@ -2,8 +2,8 @@ class VoiceSpeech {
     constructor(speechStarted, speechEnded) {
         this.myVoice = new p5.Speech('Google हिन्दी'); 
         this.myVoice.setVolume(1); 
-        this.myVoice.setRate(0.70);
-        this.myVoice.setPitch(0.92);
+        this.myVoice.setRate(0.6);
+        this.myVoice.setPitch(1.0);
         this.myVoice.utterance.onerror = this.error; 
         this.myVoice.onStart = speechStarted; 
         this.myVoice.onEnd = speechEnded;
